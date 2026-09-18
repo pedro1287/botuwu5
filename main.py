1d1f1a4f51b0e71d34c90defa04a1a0f804ea249
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2026-09-18 09:08:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -965,7 +965,7 @@ def onmessage(update,bot:ObigramClient):
   
 
 def main():
-    bot_token = '5823282217:AAHQYY5Yu1g6k6_mzrxXYnCjEoNP_ar0WXQ'
+    bot_token = '5896424509:AAHMwBUfQkHPY-xtVNdDVflLgRf0DkwrI-U'
     
 
     bot = ObigramClient(bot_token)
