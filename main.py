61ada1b390caedcd7384a35d10f8081ce04a9544
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2026-09-18 18:51:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -974,9 +974,6 @@ def main():
     asyncio.run()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
-
     try:
         main()
     except:
